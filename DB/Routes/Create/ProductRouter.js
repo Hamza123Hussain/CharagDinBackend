d185748d684ad398a107e6b66b71c2e5503fc687
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 02:08:14 +0500

    Adding THE Update and Delete Requests

--- DB/Routes/Create/ProductRouter.js
+++ DB/Routes/Create/ProductRouter.js
@@ -1,7 +1,12 @@
 import express, { Router } from 'express'
 import { ProductMaker } from '../../Controllers/Create/Product.js'
+import { ProductUpdate } from '../../Controllers/Update/UpdateProduct.js'
+import { ProductDelete } from '../../Controllers/Delete/DeleteDoc.js'
 const ProductRouter = Router()
 
 ProductRouter.post('/Create', ProductMaker)
+ProductRouter.put('/Update', ProductUpdate)
+ProductRouter.delete('/Delete/:ProductID', ProductDelete)
 
+//DEMO URL OF DELETE :http://localhost:8000/api/Product/Delete/a531ad55-e881-49fc-85fa-df995767375a?UserEmail=haniaaaaa@gmail.com
 export default ProductRouter

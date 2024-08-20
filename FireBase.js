c1df9a30602c510782d0d481d8efc6431f1ad8c1
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-20 20:50:59 +0500

    Firebase configured

---
+++ FireBase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import {
+  apiKey,
+  appId,
+  projectId,
+  authDomain,
+  messagingSenderId,
+  storageBucket,
+} from './Config.js'
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+export const Auth = getAuth(app)

da67b3fddf5d932062cc858c4c0d63a2f18fd3aa
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 01:44:14 +0500

    api to create a product, some work regarding the image url is due

---
+++ DB/Controllers/Create/Product.js
@@ -0,0 +1,32 @@
+import { doc, setDoc } from 'firebase/firestore'
+import { db } from '../../../FireBase.js'
+import { v4 as uuid } from 'uuid'
+export const ProductMaker = async (req, res) => {
+  try {
+    const { ProductName, ImageUrl, UserEmail, Category } = req.body
+
+    // Check if UserEmail and ProductName are provided
+    if (!UserEmail || !ProductName) {
+      return res
+        .status(400)
+        .json({ error: 'UserEmail and ProductName are required' })
+    }
+
+    // Optional: Sanitize UserEmail if needed
+    // const sanitizedUserEmail = UserEmail.replace(/[@.]/g, '_')
+
+    // Save the new Product to Firestore
+    await setDoc(doc(db, 'Product', ProductName), {
+      Name: ProductName,
+      MadeBY: UserEmail,
+      Category,
+      ImageUrl,
+      ID: uuid(), // Consider using a dynamic or unique ID if applicable
+    })
+
+    res.status(200).json({ ProductName, UserEmail })
+  } catch (error) {
+    console.error('Error:', error)
+    res.status(500).json({ error: 'Failed to create Product' })
+  }
+}

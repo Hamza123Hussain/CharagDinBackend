6b0f8ec9d17b8ababc87875abf82a99bb59b8f06
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 01:42:23 +0500

    allowing all the api routes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import { Port } from './Config.js'
+import CategoryRouter from './DB/Routes/Create/Category.js'
+import ProductRouter from './DB/Routes/Create/ProductRouter.js'
+import AuthRouter from './DB/Routes/Auth/AuthController.js'
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-
+app.use('/api/category', CategoryRouter)
+app.use('/api/Product', ProductRouter)
+app.use('/api/Auth', AuthRouter)
 app.listen(Port, () => {
   console.log('RUNNING ON PORT 8000')
 })

faa4779a21bc8ab7b36c60879b821401effd7e5d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 19:49:05 +0500

    using storage of firebase

--- FireBase.js
+++ FireBase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
 import {
   apiKey,
   appId,
@@ -22,3 +23,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 export const Auth = getAuth(app)
+export const Storage = getStorage(app)

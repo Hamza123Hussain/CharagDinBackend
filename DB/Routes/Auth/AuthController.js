c420e33af1b539cdcd5d36333b165e5260f73088
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 19:49:48 +0500

    uploading image file of the user

--- DB/Routes/Auth/AuthController.js
+++ DB/Routes/Auth/AuthController.js
@@ -2,9 +2,10 @@ import { Router } from 'express'
 import { SignUpController } from '../../Controllers/Auth/SignUp.js'
 import { LoginController } from '../../Controllers/Auth/Login.js'
 import { SignOutController } from '../../Controllers/Auth/SignOut.js'
+import { upload } from '../../../MulterConfig.js'
 const AuthRouter = Router()
 
-AuthRouter.post('/Register', SignUpController)
+AuthRouter.post('/Register', upload.single('image'), SignUpController)
 AuthRouter.post('/Login', LoginController)
 AuthRouter.get('/Signout', SignOutController)
 

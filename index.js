75ec9d86d6c8d6ec46cb3a31179c7048ba04fe19
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 19:48:37 +0500

    added a new Users Route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { Port } from './Config.js'
 import AuthRouter from './DB/Routes/Auth/AuthController.js'
 import ProductRouter from './DB/Routes/Product/ProductRouter.js'
 import CategoryRouter from './DB/Routes/Catorgory/Category.js'
+import UserRouter from './DB/Routes/User/UserRouter.js'
 const app = express()
 
 app.use(express.json())
@@ -12,6 +13,7 @@ app.use(cors())
 app.use('/api/category', CategoryRouter)
 app.use('/api/Product', ProductRouter)
 app.use('/api/Auth', AuthRouter)
+app.use('/api/Users', UserRouter)
 app.listen(Port, () => {
   console.log('RUNNING ON PORT 8000')
 })

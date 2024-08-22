f41653cfe214469d1964b874d744792beb8a2873
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-22 17:47:47 +0500

    fixed the update api

--- DB/Controllers/Update/UpdateProduct.js
+++ DB/Controllers/Update/UpdateProduct.js
@@ -1,31 +1,46 @@
 import { doc, updateDoc } from 'firebase/firestore'
-import { db } from '../../../FireBase.js'
-import { v4 as uuid } from 'uuid'
+import { db, Storage } from '../../../FireBase.js'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+
 export const ProductUpdate = async (req, res) => {
   try {
-    const { ProductName, ProductID, ImageUrl, UserEmail, Category } = req.body
+    const { ProductName, ProductID, UserEmail, Category, UserName } = req.body
+    const file = req.file // Image file if provided
 
-    // Check if UserEmail and ProductName are provided
-    if (!UserEmail || !ProductName) {
+    // Check if essential data is provided
+    if (!UserEmail || !ProductName || !ProductID) {
       return res
         .status(400)
-        .json({ error: 'UserEmail and ProductName are required' })
+        .json({ error: 'UserEmail, ProductName, and ProductID are required' })
     }
 
-    // Optional: Sanitize UserEmail if needed
-    // const sanitizedUserEmail = UserEmail.replace(/[@.]/g, '_')
+    let imageUrl = ''
 
-    // Save the new Product to Firestore
-    await updateDoc(doc(db, 'Product', ProductID), {
+    // Check if a file is provided and handle file upload
+    if (file) {
+      const fileRef = ref(Storage, `Product/${ProductName}_${Date.now()}`)
+      await uploadBytes(fileRef, file.buffer)
+      imageUrl = await getDownloadURL(fileRef)
+    }
+
+    // Prepare the fields to be updated
+    const updateFields = {
       Name: ProductName,
-      MadeBY: UserEmail,
+      MadeBY: UserName,
       Category,
-      ImageUrl,
-    })
+    }
+
+    // Only add ImageUrl to update fields if an image was provided
+    if (imageUrl) {
+      updateFields.ImageUrl = imageUrl
+    }
+
+    // Update the product document in Firestore
+    await updateDoc(doc(db, 'Product', ProductID), updateFields)
 
     res.status(200).json({ Message: 'PRODUCT HAS BEEN UPDATED' })
   } catch (error) {
     console.error('Error:', error)
-    res.status(500).json({ error: 'Failed to create Product' })
+    res.status(500).json({ error: 'Failed to update Product' })
   }
 }
